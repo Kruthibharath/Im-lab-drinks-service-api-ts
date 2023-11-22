ed5b4e32cb3d4bb11226537a8042ddb179ef4726
Kruthibharath <kruthi.bharath@gmail.com>
2023-11-22 13:46:18 +0000

    feat:implemented additional testcases

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,21 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+test("GET /coffee should return correct object even without param", async () => {
+  const res = await request(app).get("/coffee");
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
+test("GET /coffee should return correct object for the passed param", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Cappuccino" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Cappuccino",
+  });
+});
